c02172db3963f3b170e41be96775ec9669f1a1af
bert2 <shuairan@gmail.com>
2020-10-14 21:47:05 +0200

    add VS extension to generate the color DB

---
+++ ColorDatabaseGenerator/ColorDatabaseGeneratorPackage.cs
@@ -0,0 +1,70 @@
+﻿namespace ColorDatabaseGenerator {
+    using System;
+    using System.Drawing;
+    using System.Linq;
+    using System.Reflection;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Threading;
+
+    using Microsoft.VisualStudio.Shell;
+
+    using Task = System.Threading.Tasks.Task;
+
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [Guid(PackageGuids.PackageIdString)]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class ColorDatabaseGeneratorPackage : AsyncPackage {
+        public static readonly Entry[] DB = new[] {
+            typeof(Microsoft.VisualStudio.PlatformUI.CommonControlsColors),
+            typeof(Microsoft.VisualStudio.PlatformUI.CommonDocumentColors),
+            typeof(Microsoft.VisualStudio.PlatformUI.EnvironmentColors),
+            typeof(Microsoft.VisualStudio.PlatformUI.HeaderColors),
+            typeof(Microsoft.VisualStudio.PlatformUI.InfoBarColors),
+            typeof(Microsoft.VisualStudio.PlatformUI.ProgressBarColors),
+            typeof(Microsoft.VisualStudio.PlatformUI.SearchControlColors),
+            typeof(Microsoft.VisualStudio.PlatformUI.StartPageColors),
+            typeof(Microsoft.VisualStudio.PlatformUI.ThemedDialogColors),
+            typeof(Microsoft.VisualStudio.PlatformUI.TreeViewColors),
+            typeof(Microsoft.VisualStudio.PlatformUI.UnthemedDialogColors),
+            typeof(Microsoft.VisualStudio.PlatformUI.VisualStudioInstallerColors)
+        }
+        .SelectMany(
+            t => t.GetProperties().Where(p => p.PropertyType == typeof(ThemeResourceKey)),
+            Entry.Create)
+        .ToArray();
+
+        protected override async Task InitializeAsync(CancellationToken ct, IProgress<ServiceProgressData> progress) {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(ct);
+            await GrabDarkThemeColorsCommand.InitializeAsync(this);
+            await GrabBlueThemeColorsCommand.InitializeAsync(this);
+            await GrabLightThemeColorsCommand.InitializeAsync(this);
+        }
+    }
+
+    public enum Theme { Dark = 0, Blue = 1, Light = 2 }
+
+    public class Entry {
+        public readonly string DeclaringType;
+        public readonly ThemeResourceKey Key;
+        public Color this[Theme t] {
+            get => colors[(int)t];
+            set => colors[(int)t] = value;
+        }
+        public readonly Color[] colors = new Color[3];
+        public Entry(string declaringType, ThemeResourceKey key) {
+            DeclaringType = declaringType;
+            Key = key;
+        }
+        public static Entry Create(Type declaringType, PropertyInfo keyProp) => new Entry(
+            declaringType.Name,
+            (ThemeResourceKey)keyProp.GetValue(null));
+        public override string ToString() => new StringBuilder(capacity: 100)
+            .Append(DeclaringType).Append('.').Append(Key.Name).Append(": ")
+            .Append(Hex(this[Theme.Dark])).Append(" (d), ")
+            .Append(Hex(this[Theme.Blue])).Append(" (b), ")
+            .Append(Hex(this[Theme.Light])).Append(" (l)")
+            .ToString();
+        private static string Hex(Color c) => c.Name == "0" ? "n/a" : c.Name.Substring(2);
+    }
+}
---
+++ ColorDatabaseGenerator/GrabLightThemeColorsCommand.cs
@@ -0,0 +1,43 @@
+﻿namespace ColorDatabaseGenerator {
+    using System.ComponentModel.Design;
+
+    using Microsoft;
+    using Microsoft.VisualStudio.Shell;
+    using Microsoft.VisualStudio.Shell.Interop;
+
+    using Task = System.Threading.Tasks.Task;
+
+    internal sealed class GrabLightThemeColorsCommand {
+        public static async Task InitializeAsync(AsyncPackage pkg) {
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(pkg.DisposalToken);
+
+            var cmdService = await pkg.GetServiceAsync<IMenuCommandService, IMenuCommandService>();
+            Assumes.Present(cmdService);
+
+            var cmdId = new CommandID(PackageGuids.CmdSetId, PackageIds.GrabLightThemeColorsCommandId);
+            var cmd = new OleMenuCommand(
+                (_, __) => ThreadHelper.JoinableTaskFactory.RunAsync(async delegate {
+                    await Execute(pkg);
+                }),
+                cmdId);
+
+            cmdService.AddCommand(cmd);
+        }
+
+        public static async Task Execute(AsyncPackage pkg) {
+            var shell = await pkg.GetServiceAsync<SVsUIShell, IVsUIShell5>();
+
+            foreach (var entry in ColorDatabaseGeneratorPackage.DB)
+                entry[Theme.Light] = shell.GetThemedGDIColor(entry.Key);
+
+            _ = VsShellUtilities.ShowMessageBox(
+                pkg,
+                "Light theme colors stored in memory.",
+                "Done!",
+                OLEMSGICON.OLEMSGICON_INFO,
+                OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST
+            );
+        }
+    }
+}
